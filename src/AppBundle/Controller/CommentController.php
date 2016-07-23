985696d811633fb6eaa7c8838e267e7402c3b495
sohrab ghazinia <sohrab.ghazinia@me.com>
2016-07-23 20:38:59 +0200

    send email on comment posted

--- src/AppBundle/Controller/CommentController.php
+++ src/AppBundle/Controller/CommentController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CommentController extends SuperController
 {
@@ -22,9 +23,11 @@ class CommentController extends SuperController
         $comment = new Comment();
         $form = $this->createForm(CommentType::class,$comment);
         $form->handleRequest($request);
+
         if($form->isValid()){
             $em->persist($comment);
             $em->flush();
+            $this->sendAdminMail($comment);
         }
 
         if($request->isXmlHttpRequest()){
@@ -52,6 +55,53 @@ class CommentController extends SuperController
 
         exit;
 
+    }
+
+    private function sendAdminMail(Comment $comment){
+
+        $email  = $this->get('app.application.service')->getParameter("site_email");
+
+        if(!$email || empty($email))
+            return false;
+
+        $em         = $this->getDoctrine()->getManager();
+        $mailer     = $this->get('mailer');
+        $object     = $em->getRepository("AppBundle:".$comment->getParentClass())->find($comment->getParentId());
+
+        $objectLink = $this->generateUrl('easyadmin',
+            array(
+                'action' => 'show',
+                'entity' => ucfirst($comment->getParentClass()),
+                'id' =>$comment->getParentId()
+            )
+            ,UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        $data   = array(
+            'comment'       => $comment,
+            'object'        => $object,
+            'objectLink'    => $objectLink
+        );
+
+        if($comment->getAuthor() != "Anonymous"){
+            $author             = $em->getRepository("AppBundle:User")->findOneBy(['username'=>$comment->getAuthor()]);
+            $data['author']     = $author;
+        }
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject("New comment posted on ".$comment->getParentClass()." : ".$comment->getParentId())
+            ->setFrom($email)
+            ->setReplyTo($email)
+            ->setTo($email)
+            ->setBody(
+                $this->renderView(
+                    $this->templating('emails/comment.html.twig'),
+                    $data
+                ),
+                'text/html'
+            )
+        ;
+        $mailer->send($message);
     }
 
 }
